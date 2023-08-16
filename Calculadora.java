7856755930ebe8c2531ae59f234ab637bd5a77a7
Alejoox08 <141367683+Alejoox08@users.noreply.github.com>
2023-08-16 18:06:22 -0500

    Update Calculadora.java

--- Calculadora.java
+++ Calculadora.java
@@ -1,11 +1,14 @@
-    public static double realizarSeno(Scanner scanner) {
+    public static double realizarTangente(Scanner scanner) {
         System.out.print("Digite el angulo en grados: ");
         double angulo = Math.toRadians(scanner.nextDouble());
-        return Math.sin(angulo);
+        return Math.tan(angulo);
     }
     
-    public static double realizarCoseno(Scanner scanner) {
-        System.out.print("Digite el angulo en grados: ");
-        double angulo = Math.toRadians(scanner.nextDouble());
-        return Math.cos(angulo);
+    public static double realizarRaiz(Scanner scanner) {
+        System.out.print("Digite el numero: ");
+        double num = scanner.nextDouble();
+        System.out.print("Digite el indice de la raiz: ");
+        double indice = scanner.nextDouble();
+        
+        return Math.pow(num, 1.0 / indice);
     }
