2d651fc513311bdde3532b262e89d3a9262a7867
Alejoox08 <141367683+Alejoox08@users.noreply.github.com>
2023-08-16 18:00:16 -0500

    Update Calculadora.java

--- Calculadora.java
+++ Calculadora.java
@@ -1,15 +1,21 @@
-    public static double realizarSuma(Scanner scanner) {
-        System.out.print("Digite el primer numero: ");
-        double num1 = scanner.nextDouble();
-        System.out.print("Digite el segundo numero: ");
-        double num2 = scanner.nextDouble();
-        return num1 + num2;
-    }
-    
-    public static double realizarResta(Scanner scanner) {
+    public static double realizarMultiplicacion(Scanner scanner) {
         System.out.print("Digite el primer número: ");
         double num1 = scanner.nextDouble();
         System.out.print("Digite el segundo número: ");
         double num2 = scanner.nextDouble();
-        return num1 - num2;
+        return num1 * num2;
+    }
+    
+    public static double realizarDividision(Scanner scanner) {
+        System.out.print("Digite el dividendo: ");
+        double dividendo = scanner.nextDouble();
+        System.out.print("Digite el divisor: ");
+        double divisor = scanner.nextDouble();
+        
+        if (divisor == 0) {
+            System.out.println("Ups, no es posible dividir entre cero. ");
+            return 0;
+        }
+        
+        return dividendo / divisor;
     }
